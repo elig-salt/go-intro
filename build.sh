0d1577f46c6b3bfd67f419c72dc6aca61ddae2ae
Eli Goldberg <elig@salt.security>
2021-02-09 13:17:01 +0200

    time build

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-while IFS= read -r -d '' dir; do
-  # echo $dir
-	binary_name=$(echo $dir | sed -e 's/\(.* - \)*//g')
-  # echo $binary_name
+time while IFS= read -r -d '' dir; do
+  binary_name=$(echo $dir | sed -e 's/\(.* - \)*//g')
 	go build -o bin/$binary_name "$dir"
 done < <(find . -type d -name "* - *" -print0)
